9f93a5c9ba225429e24ec0e8fbaf7ff01d24ef63
Austin Kelsay <53542748+AustinKelsay@users.noreply.github.com>
2022-09-02 20:20:41 +0000

    Initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
-import Header from "./components/Header"
-import Blocks from "./components/Blocks"
 import "./App.css"
 
 // This is our main App 'component' which gets inserted into the standard webpage
-// Anything we want in our
-// After importing our 'child' components we can render them in the App view
+// After importing our 'child' components we can render them in the App view below
 export default function App() {
   return (
     <main>
-      <Header />
-      <Blocks /> 
+      <h1>Hello world!</h1>
     </main>
   )
 }
